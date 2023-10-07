b4268c212bebc034f2c4cdf56ba3b79738ae3c84
Antonio-Giorg <147244339+Antonio-Giorg@users.noreply.github.com>
2023-10-07 12:09:20 +0200

    Update Program.cs

--- homework-1/homework-1-c_sharp/Program.cs
+++ homework-1/homework-1-c_sharp/Program.cs
@@ -13,23 +13,21 @@ class Program
             // Creare un oggetto Graphics dall'immagine
             using (Graphics g = Graphics.FromImage(bitmap))
             {
-                // Disegnare un punto rosso alle coordinate (50, 50)
+                // Disegno un punto 
                 g.FillEllipse(Brushes.Red, 50, 50, 10, 10);
 
-                // Disegnare una linea blu dalle coordinate (100, 100) a (200, 200)
+                // Disegno una linea blu 
                 g.DrawLine(Pens.Blue, 100, 100, 200, 200);
 
-                // Disegnare un rettangolo verde con angolo in alto a sinistra alle coordinate (250, 100)
-                // e larghezza 100 e altezza 50
+                // Disegno un rettangolo verde
                 g.FillRectangle(Brushes.Green, 250, 100, 100, 50);
 
-                // Disegnare un cerchio arancione con centro alle coordinate (400, 150)
-                // e raggio 50
+                // Disegno un cerchio arancione
                 g.FillEllipse(Brushes.Orange, 350, 100, 100, 100);
             }
 
-            // Salvare l'immagine in un file (puoi anche visualizzarla o utilizzarla come desideri)
-            bitmap.Save("D:\\magistrale\\statistics\\output.png");
+            // Salvare l'immagine in un file
+            bitmap.Save("C:\\output.png");
         }
     }
 }
